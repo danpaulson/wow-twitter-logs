0ce8da893683122359951dac082b3f9ec997a489
Dan Paulson <danpaulson@gmail.com>
2013-07-14 20:29:01 -0400

    Make sure to get all logs for the day

--- wow-twitter-logs.py
+++ wow-twitter-logs.py
@@ -33,42 +33,44 @@ if len(logs) == 0:
 day = today.strftime("%d-%m")
 
 # Get that night's log page
-url = 'http://worldoflogs.com%s' % logs[0].get('href')
 response = urllib2.urlopen(url)
 soup = BeautifulSoup(response.read())
 
-attempts = soup.find(text='Bosses').findNext('ul').findAll('a')
-ranked = soup.find('table', {'class': 'playerRankMixed'}).findAll('tr')
+for log in logs:
+    url = 'http://worldoflogs.com%s' % logs[0].get('href')
 
-# Check for new logs, tweet
-for attempt in attempts:
-    if 'bosses' not in attempt['href']:
-        added = r.sadd(day, attempt.string[:-8])
-        attempt_type = 'Wipe' if 'Try' in attempt.string else 'Kill'
+    attempts = soup.find(text='Bosses').findNext('ul').findAll('a')
+    ranked = soup.find('table', {'class': 'playerRankMixed'}).findAll('tr')
 
+    # Check for new logs, tweet
+    for attempt in attempts:
+        if 'bosses' not in attempt['href']:
+            added = r.sadd(day, attempt.string[:-8])
+            attempt_type = 'Wipe' if 'Try' in attempt.string else 'Kill'
+
+            if added:
+                status = '#%s %s - http://www.worldoflogs.com%s' % (
+                        attempt_type,
+                        parser.unescape(attempt.string),
+                        attempt['href'])
+
+                twitter.update_status(status=status)
+
+                if VERBOSE:
+                    print 'Tweeted: %s' % status
+
+    # Remove the header
+    ranked.pop(0)
+
+    for ranking in ranked:
+        boss = ranking.findAll('td')[2].contents[0]
+        dps = ranking.findAll('td')[5].contents[0]
+        rank = ranking.find('span').contents
+        link = ranking.find('a')
+
+        added = r.sadd('%s_ranked' % day, '%s_%s' % (boss, link.string))
         if added:
-            status = '#%s %s - http://www.worldoflogs.com%s' % (
-                    attempt_type,
-                    parser.unescape(attempt.string),
-                    attempt['href'])
-
+            status = '#Rank %s on %s by %s (%s)' % (rank[0], boss, link.string, dps)
             twitter.update_status(status=status)
-
             if VERBOSE:
                 print 'Tweeted: %s' % status
-
-# Remove the header
-ranked.pop(0)
-
-for ranking in ranked:
-    boss = ranking.findAll('td')[2].contents[0]
-    dps = ranking.findAll('td')[5].contents[0]
-    rank = ranking.find('span').contents
-    link = ranking.find('a')
-
-    added = r.sadd('%s_ranked' % day, '%s_%s' % (boss, link.string))
-    if added:
-        status = '#Rank %s on %s by %s (%s)' % (rank[0], boss, link.string, dps)
-        twitter.update_status(status=status)
-        if VERBOSE:
-            print 'Tweeted: %s' % status
