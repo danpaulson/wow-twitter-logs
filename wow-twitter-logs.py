e91181f75c414de4dfc781829ea30f36f5d3e118
Dan Paulson <danpaulson@gmail.com>
2013-12-29 15:26:39 -0500

    Add percentages to wipes where applicable, add debug mode

--- wow-twitter-logs.py
+++ wow-twitter-logs.py
@@ -1,3 +1,4 @@
+from __future__ import division
 from local_settings import *
 
 import urllib2
@@ -28,7 +29,7 @@ if len(logs) == 0:
     logs = soup.find(text=today.strftime("%d-%m")).findNext('div').findAll('a')
 
     if len(logs) == 0:
-        sys.exit('There are no logs yet today.')
+        sys.exit('There are no logs today.')
 
 day = today.strftime("%d-%m")
 
@@ -48,15 +49,34 @@ for log in logs:
             attempt_type = 'Wipe' if 'Try' in attempt.string else 'Kill'
             description = parser.unescape(attempt.string)
             raid_format_check = RAID_FORMAT in description
-            boss_check = True in [boss in description for boss in TRACKED_BOSSES]
+            boss_check = True in [boss in description for boss in TRACKED_BOSSES.keys()]
+
+            # Find Boss name
+            for name in TRACKED_BOSSES.keys():
+                if name in description:
+                    boss_name = name
 
             if added and raid_format_check and boss_check:
+                if attempt_type == 'Wipe':
+                    url = 'http://www.worldoflogs.com%s' % attempt['href']
+                    response = urllib2.urlopen(url)
+                    soup = BeautifulSoup(response.read())
+
+                    damage_table = soup.find(text='Damage done by target').findNext('table').find(text=boss_name).parent.parent.parent.findNext('td').text
+                    damage_done = int(damage_table.replace(' ',''))
+                    damage_total = TRACKED_BOSSES[boss_name]
+
+                    if damage_total > 0:
+                        percentage = int(damage_done) / int(damage_total)
+                        attempt_type = '%s (%s%%)' % (attempt_type, int(percentage*100))
+
                 status = '#%s %s - http://www.worldoflogs.com%s' % (
                         attempt_type,
                         description,
                         attempt['href'])
 
-                twitter.update_status(status=status)
+                if not DEBUG:
+                    twitter.update_status(status=status)
 
                 if VERBOSE:
                     print 'Tweeted: %s' % status
@@ -79,6 +99,7 @@ for log in logs:
             added = r.sadd('%s_ranked' % day, '%s_%s' % (boss, link.string))
             if added:
                 status = '#Rank %s on %s by %s (%s)' % (rank[0], boss, link.string, dps)
-                twitter.update_status(status=status)
+                if not DEBUG:
+                    twitter.update_status(status=status)
                 if VERBOSE:
                     print 'Tweeted: %s' % status
